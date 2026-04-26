c6b40433653430ada5c8a1d07139256b1eb85f58
4mu3l <samuelchocolate1@gmail.com>
2026-04-25 22:14:10 -0300

    API_SAGE

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sage.db"
-engine = create_engine(
- SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SenssionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
-Base = declarative_base()
